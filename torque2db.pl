0778c726728d0760c1c9051002ba5e754f4f8364
Paul Cochrane <cochrane@rrzn.uni-hannover.de>
2013-08-19 15:41:03 +0200

    Moved description of data into POD

--- torque2db.pl
+++ torque2db.pl
@@ -4,6 +4,67 @@
 
 torque2db - save Torque (PBS) accounting data into a database
 
+=head1 SYNOPSIS
+
+torque2db
+
+=head1 DESCRIPTION
+
+Processes the data in C</var/spool/torque/server_priv/accounting> and saves
+this information into an SQLite database for further processing with other
+tools.
+
+=head2 Data Description
+
+Here is the meaning of the fields in the database
+
+=over 4
+
+=item jobid                Job ID
+
+=item user                 Username
+
+=item ugroup               User group
+
+=item queue                Queue name
+
+=item queue_time           Job queue time (in seconds since epoch)
+
+=item start_time           Job start time (in seconds since epoch)
+
+=item completion_time      Job end time   (in seconds since epoch)
+
+=item exec_host            Where the job executed (as a string)
+
+=item used_cputime         CPU time used by job
+
+=item allocated_tasks      How many processors were allocated to the job
+
+=item required_walltime    How much walltime was requested (seconds)
+
+=item used_walltime        Walltime actually used (seconds)
+
+=item required_memory      RAM requested by job (kB)
+
+=item used_memory          RAM used by the job (kB)
+
+=item used_virtual_memory  Virtual memory used by the job (kB)
+
+=item exit_status          Torque exit status
+
+=back
+
+=head1 AUTHOR
+
+Paul Cochrane
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 Paul Cochrane
+
+This library is free software; you may redistribute it and/or modify
+it under the same terms as Perl itself.
+
 =cut
 
 use warnings;
@@ -51,24 +112,6 @@ my @rows  = qw(jobid user ugroup queue queue_time start_time completion_time
                 exec_host used_cputime required_memory allocated_tasks
                 required_ncpus required_walltime used_memory
                 used_virtual_memory exit_status);
-
-# here is the meaning of the fields in the db
-# jobid               Job ID
-# user                Username
-# ugroup              User group
-# queue               Queue name
-# queue_time          Job queue time (in seconds since epoch)
-# start_time          Job start time (in seconds since epoch)
-# completion_time     Job end time   (in seconds since epoch)
-# exec_host           Where the job executed (as a string)
-# used_cputime        CPU time used by job
-# allocated_tasks     How many processors were allocated to the job
-# required_walltime   How much walltime was requested (seconds)
-# used_walltime       Walltime actually used (seconds)
-# required_memory     RAM requested by job (kB)
-# used_memory         RAM used by the job (kB)
-# used_virtual_memory Virtual memory used by the job (kB)
-# exit_status         Torque exit status
 
 # create the table if it doesn't already exist
 my $create_table_string = <<"EOD";
