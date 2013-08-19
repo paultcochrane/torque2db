613cc83162235e9ebafd0ba97d6df836364bf8a8
Paul Cochrane <cochrane@rrzn.uni-hannover.de>
2013-08-19 15:15:38 +0200

    Creating an sqlite table with the relevant data

--- torque2db.pl
+++ torque2db.pl
@@ -10,6 +10,8 @@ use warnings;
 use strict;
 use autodie;
 
+use DBI;
+
 use Job;
 use User;
 
@@ -34,6 +36,64 @@ for my $line ( @executed_job_data ) {
     $jobs{$job->jobid} = $job;
 }
 
-print scalar keys ( %jobs ), "\n";
+my $dbfile = 'torque.db';      # your database file
+my $dbh = DBI->connect(        # connect to your database, create if needed
+        "dbi:SQLite:dbname=$dbfile", # DSN: dbi, driver, database file
+        "",                          # no user
+        "",                          # no password
+        { RaiseError => 1,           # complain if something goes wrong
+          AutoCommit => 1,           # commit changes to db automatically
+        },
+                ) or die $DBI::errstr;
+
+my $table = 'jobs';
+my @rows  = qw(jobid user ugroup queue queue_time start_time completion_time
+                exec_host used_cputime required_memory allocated_tasks
+                required_ncpus required_walltime used_memory
+                used_virtual_memory exit_status);
+
+# here is the meaning of the fields in the db
+# jobid               Job ID
+# user                Username
+# ugroup              User group
+# queue               Queue name
+# queue_time          Job queue time (in seconds since epoch)
+# start_time          Job start time (in seconds since epoch)
+# completion_time     Job end time   (in seconds since epoch)
+# exec_host           Where the job executed (as a string)
+# used_cputime        CPU time used by job
+# allocated_tasks     How many processors were allocated to the job
+# required_walltime   How much walltime was requested (seconds)
+# used_walltime       Walltime actually used (seconds)
+# required_memory     RAM requested by job (kB)
+# used_memory         RAM used by the job (kB)
+# used_virtual_memory Virtual memory used by the job (kB)
+# exit_status         Torque exit status
+
+# create the table if it doesn't already exist
+my $create_table_string = <<"EOD";
+create table $table ( id INT PRIMARY KEY,
+                      user TEXT,
+                      ugroup TEXT,
+                      queue TEXT,
+                      queue_time INT,
+                      start_time INT,
+                      completion_time INT,
+                      exec_host TEXT,
+                      used_cputime INT,
+                      allocated_tasks INT,
+                      required_walltime INT,
+                      used_walltime INT,
+                      required_memory INT,
+                      used_memory INT,
+                      used_virtual_memory INT,
+                      exit_status INT
+                    )
+EOD
+
+$dbh->do("drop table if exists $table");
+$dbh->do($create_table_string) or die $DBI::errstr;
+
+$dbh->disconnect();
 
 # vim: expandtab shiftwidth=4
