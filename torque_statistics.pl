d224875f344d557b1fd12cd8535502ec877cd840
Paul Cochrane <cochrane@rrzn.uni-hannover.de>
2013-09-25 18:47:44 +0200

    Added docs describing the torque_statistics program

--- torque_statistics.pl
+++ torque_statistics.pl
@@ -1,5 +1,31 @@
 #!/usr/bin/env perl
 
+=head1 NAME
+
+torque_statistics - generate cluster statistics from Torque accounting data
+
+=head1 SYNOPSIS
+
+torque_statistics
+
+=head1 DESCRIPTION
+
+Processes the data in C</var/spool/torque/server_priv/accounting> to
+generate cluster usage statistics.
+
+=head1 AUTHOR
+
+Paul Cochrane
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 Paul Cochrane
+
+This library is free software; you may redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
 package torque_statistics;
 
 use warnings;
